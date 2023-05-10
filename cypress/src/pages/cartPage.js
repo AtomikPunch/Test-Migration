481b05822a3a622ca4900086d725c84145a6ef1a
Salim STASAID <salim.stasaid@b-acceptance.com>
2023-05-10 20:45:44 +0200

    Déplacement des objets de la page formulaire de création de compte de cartPage vers accountPage

--- cypress/src/pages/cartPage.js
+++ cypress/src/pages/cartPage.js
@@ -1,50 +1,30 @@
-exports.cartPage = class cartPage{
+exports.cartPage = class cartPage {
 
-    get check_in(){
-        return cy.get('.ds-ens-prices-bottom__action', {timeout : 5000})
+    get check_in() {
+        return cy.get('.ds-ens-prices-bottom__action', { timeout: 5000 })
     }
 
-    get checkbox_checked(){
-        return cy.get('[data-test*="click_and_collect"] .idf-radio__field', {timeout : 5000});
+    get checkbox_checked() {
+        return cy.get('[data-test*="click_and_collect"] .idf-radio__field', { timeout: 5000 });
     }
 
-    get create_new_account(){
-        return cy.get('.create-account', {timeout : 5000});
+    get create_new_account() {
+        return cy.get('.create-account', { timeout: 5000 });
     }
 
-    get last_name_input(){
-        return cy.get('[data-test="v-create-account-last-name"] input' , {timeout : 5000})
+    get reduced_price() {
+        return cy.get('.ds-ens-product-card-line__price .ds-ens-pricing__price-amount--xxl', { timeout: 5000 });
     }
 
-    get first_name_input(){
-        return cy.get('[data-test="v-create-account-first-name"] input', {timeout : 5000});
+    get total_price() {
+        return cy.get('.ds-ens-prices-bottom__total .ds-ens-pricing', { timeout: 5000 });
     }
 
-    get email_input(){
-        return cy.get('[data-test="v-create-account-email"] input', {timeout : 5000});
+    get empty_cart() {
+        return cy.get('.empty-cart', { timeout: 5000 });
     }
 
-    get password_input(){
-        return cy.get('[data-test="c-password-input"]' , {timeout : 5000});
-    }
-
-    get submit_account_creation(){
-        return cy.get('[data-test="v-create-account-submit-button"]', {timeout : 5000});
-    }
-
-    get reduced_price(){
-        return cy.get('.ds-ens-product-card-line__price .ds-ens-pricing__price-amount--xxl', {timeout : 5000});
-    }
-
-    get total_price(){
-        return cy.get('.ds-ens-prices-bottom__total .ds-ens-pricing', {timeout : 5000});
-    }
-
-    get empty_cart(){
-        return cy.get('.empty-cart', {timeout : 5000});
-    }
-
-    get checkbox_home_delivery(){
-        return cy.get('[data-test*="home_delivery"] .idf-radio__frame', {timeout : 5000});
+    get checkbox_home_delivery() {
+        return cy.get('[data-test*="home_delivery"] .idf-radio__frame', { timeout: 5000 });
     }
 }
