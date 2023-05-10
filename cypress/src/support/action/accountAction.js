7464b54ed61b4db31ac0b0eede129e8d93a56e8d
Salim STASAID <salim.stasaid@b-acceptance.com>
2023-05-10 20:36:10 +0200

    MAJ de la commande i_create_a_new_account_by_filling_form ==> refactoring car j'ai déplacé les objets dans une autre page pour pouvoir les réutiliser et ne pas les dupliquer
    Ajout des commandes :
    i_create_a_new_account_from_header ==> pour créer un nouveau utilisateur depuis le header
    generate_email_random ==> générer une adresse email aléatoire pour les cas ou on a besoin de créer un compte

--- cypress/src/support/action/accountAction.js
+++ cypress/src/support/action/accountAction.js
@@ -4,7 +4,7 @@ Cypress.Commands.add("i_fill_the_login_form", (client_reference) => {
         bag.data.clients.last = client;
 
         cy.log("i_access_to_the_webstore as " + client_reference);
-        
+
         bag.pages.signon.login.type(client.email);
         bag.pages.signon.password.type(client.password);
     });
@@ -13,10 +13,10 @@ Cypress.Commands.add("i_fill_the_login_form", (client_reference) => {
 Cypress.Commands.add("i_submit_the_login_form", () => {
     cy.get("@bag").then((bag) => {
         cy.log("i_submit_the_login_form");
-        
+
         cy.intercept("POST", "https://invivo-jardiland-test.eu.auth0.com/oauth/token").as("loginResponse");
         bag.pages.signon.login_submit.click();
-        
+
         cy.wait("@loginResponse").then((intercept) => {
             bag.data.clients.last.access_token = intercept.response.body.access_token;
             // #HACK : We should not have to accept th cookies twice 
@@ -36,11 +36,51 @@ Cypress.Commands.add("i_create_a_new_account_by_filling_form", (client_reference
     cy.get("@bag").then((bag) => {
         let client = bag.data.clients[client_reference]
         cy.log("i_create_a_new_account");
-        bag.pages.cart.create_new_account.click();
-        bag.pages.cart.first_name_input.clear().type(client.first_name);
-        bag.pages.cart.last_name_input.type(client.last_name);
-        bag.pages.cart.email_input.type(client.email);
-        bag.pages.cart.password_input.type(client.password);
-        bag.pages.cart.submit_account_creation.click();
+        bag.pages.cart.create_new_account.trigger('click');
+        bag.pages.account.first_name_input.clear().type(client.first_name);
+        bag.pages.account.last_name_input.type(client.last_name);
+        bag.pages.account.email_input.type(client.email);
+        bag.pages.account.password_input.type(client.password);
+        bag.pages.account.submit_account_creation.click();
+        bag.pages.account.successfulConnexion.should('exist');
+
+        // #HACK : We should not have to accept th cookies twice 
+        bag.pages.commons.accept_cookies.click();
     });
 })
+
+Cypress.Commands.add("i_create_a_new_account_from_header", (client_reference) => {
+    cy.get("@bag").then((bag) => {
+        let client = bag.data.clients[client_reference]
+        cy.log("i_create_new_account_from_header");
+        bag.pages.account.first_name_input.clear().type(client.first_name);
+        bag.pages.account.last_name_input.type(client.last_name);
+        bag.pages.account.email_input.type(client.email);
+        bag.pages.account.password_input.type(client.password);
+        bag.pages.account.submit_account_creation.click();
+
+        // #HACK : We should not have to accept th cookies twice 
+        bag.pages.commons.accept_cookies.click();
+    })
+})
+
+
+Cypress.Commands.add("generate_email_random", (client_reference) => {
+    function generateRandomEmail() {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let randomString = '';
+        for (let i = 0; i < 10; i++) {
+            randomString += characters[Math.floor(Math.random() * characters.length)];
+        }
+        return `test.${randomString}@getnada.com`;
+    }
+
+    cy.readFile('cypress/resources/dataset.json').then((data) => {
+        const randomEmail = generateRandomEmail();
+        let client = data.clients[client_reference]
+        client.email = randomEmail;
+        cy.writeFile('cypress/resources/dataset.json', JSON.stringify(data));
+    })
+})
+
+
