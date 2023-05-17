66be677d416bdf1141ec2242f340c07d86881387
Jad <jad.yamout@b-acceptance.com>
2023-05-17 16:02:35 +0200

    Add: added SH04, SH05 , SH06 Add : added method linked to SH04 , SH05 , SH06 in cucumber format

--- cypress/support/step_definitions/storeAction.js
+++ cypress/support/step_definitions/storeAction.js
@@ -32,10 +32,11 @@ Cypress.Commands.add('i_fill_search_store',(store_reference) =>{
     });
 })
 
+Then("Je choisi {string} comme magasin favoris", (store_reference) => {cy.i_select_favorite_store(store_reference);})
 Cypress.Commands.add('i_select_favorite_store', (store_reference) => {
     cy.get("@bag").then((bag) => {
         let store = bag.data.store[store_reference]
-        cy.get('[data-test-store-locator-modal-card="'+store.card_number+'"] .ens-store-locator-card__cta').should('be.visible').click();
+        cy.get('[data-test-store-locator-modal-card="'+store.store_id+'"] .ens-store-locator-card__cta').should('be.visible').click();
         bag.pages.commons.store_name.invoke('text').then((text) =>{
             expect(text.trim()).equal(store.name);
         });
